3c4df696b4b06095f5f75c972ebb19b0b6dcaca6
sulav khadka <sulav.khadka787@gmail.com>
2023-03-21 11:25:18 -0600

    declaring variables

---
+++ odin-links-and-images/js/01_day/01.js
@@ -0,0 +1,24 @@
+let message;
+console.log(23+97);
+console.log(1+3+5+7+8+9);
+console.log((4+6+9)/77);
+console.log('============');
+
+let a=10;
+console.log(a);
+console.log(9*a);
+let b=7*a;
+console.log(b);
+
+
+console.log('2========');
+
+const max=57;
+
+let actual=max-13;
+
+let percentage=actual/max;
+
+console.log('percentage = ',percentage );
+
+console.log('git hub update not wworking so adding comment to test');
